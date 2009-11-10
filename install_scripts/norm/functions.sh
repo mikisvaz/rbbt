46db00ac4fd9489cd0e4b86787b054ca6d4abd3e
Miguel Vazquez <miguel.vazquez@fdi.ucm.es>
2009-11-10 19:18:59 +0100

    Source format

--- install_scripts/norm/functions.sh
+++ install_scripts/norm/functions.sh
@@ -1,21 +1,23 @@
 #!/bin/bash
 function norm(){
-    o=$1
+    organism=$1
     shift
-    s=$1
+    dataset=$1
     shift
-    n=$1
+    ner=$1
     shift
 
-    echo "rm results/${o}_$s; rake results/${o}_$s.eval ner=$n $@ > ${o}_$s.log_$n; tail results/${o}_$s.eval"
-    rm results/${o}_$s; rake results/${o}_$s.eval ner=$n $@ > ${o}_$s.log_$n; tail results/${o}_$s.eval
+    CMD="rm results/${organism}_$dataset; rake results/${organism}_$dataset.eval ner=$ner $@ > ${organism}_$dataset.log_$ner; tail results/${organism}_$dataset.eval"
+    echo $CMD
+    $CMD
 }
 
 
 function norm_2(){
-    n=$1
+    ner=$1
     shift
 
-    echo "rm results/bc2gn; rake results/bc2gn.eval ner=$n $@ > bc2gn.log_$n; tail results/bc2gn.eval"
-    rm results/bc2gn; rake results/bc2gn.eval ner=$n $@ > bc2gn.log_$n; tail results/bc2gn.eval
+    CMD="rm results/bc2gn; rake results/bc2gn.eval ner=$ner $@ > bc2gn.log_$ner; tail results/bc2gn.eval"
+    echo $CMD
+    $CMD
 }
