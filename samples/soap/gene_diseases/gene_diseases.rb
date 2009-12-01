62b6771cdc49ee63716cb17d70bb45f4f11b1005
Miguel Vazquez <miguel.vazquez@fdi.ucm.es>
2009-12-01 23:59:01 +0100

    Add client and doc for gene_diseases

---
+++ samples/soap/gene_diseases/gene_diseases.rb
@@ -0,0 +1,34 @@
+require 'simplews/rake'
+
+class GeneDiseasesWS < SimpleWS::Jobs
+
+  # Gene list must be in Entrez Gene id format
+  task :diseases, %w(list), {:list => :array, :return => :hash}, ['diseases/{JOB}'] do |list|
+    
+    # Add some status messages for different rake subtasks. Task defined as
+    # rules cannot be described using the desc method in rake.
+    add_message('pmids', :pmids, "Listing article pmids for genes in the list")
+    add_message('metadoc', :metadoc, "Joining article abstracts in meta-document")
+    add_message('diseases', :disease, "Finding disease terms in meta-document")
+
+
+    # Prepare input data for the pipeline using a global variable
+    $genes = list
+
+    # Instruct rake to produce the file
+    rake
+  end
+
+end
+
+if __FILE__ == $0
+
+  # Create directories for intemediate results
+  %w(pmids metadoc diseases).collect{|dir| File.join('work', dir)}.each{|dir|
+    FileUtils.mkdir_p dir unless File.exist? dir
+  }
+
+  # Launch the web service
+  GeneDiseasesWS.new('GeneDiseasesWS', 'Find diseases associated to a list of genes', 'localhost', 8081, 'work').start
+end
+
---
+++ samples/soap/gene_diseases/gene_diseases_client.rb
@@ -0,0 +1,20 @@
+require 'simplews'
+
+# Gene list must be in Entrez Gene id format
+
+genes = STDIN.read.split
+
+driver = SimpleWS.get_driver('http://localhost:8081', 'GeneDiseasesWS')
+
+job = driver.diseases(genes, 'test_client')
+
+while ! driver.done job
+  puts "Status: #{driver.status job}"
+  sleep 5
+end
+
+raise "Error: #{driver.messages(job).last}" if driver.error job
+
+puts driver.result(driver.results(job))
+
+
