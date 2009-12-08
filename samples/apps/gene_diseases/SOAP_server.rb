a9ec47691e289d2b4fc02993bf873985be5799f0
Miguel Vazquez <miguel.vazquez@fdi.ucm.es>
2009-12-08 19:55:54 +0100

    Remove return type, not used in tasks. Change metadoc to metadocs dir

--- samples/apps/gene_diseases/SOAP_server.rb
+++ samples/apps/gene_diseases/SOAP_server.rb
@@ -3,7 +3,7 @@ require 'simplews/rake'
 class GeneDiseasesWS < SimpleWS::Jobs
 
   # Gene list must be in Entrez Gene id format
-  task :diseases, %w(list), {:list => :array, :return => :hash}, ['diseases/{JOB}'] do |list|
+  task :diseases, %w(list), {:list => :array}, ['diseases/{JOB}'] do |list|
     
     # Add some status messages for different rake subtasks. Task defined as
     # rules cannot be described using the desc method in rake.
@@ -24,7 +24,7 @@ end
 if __FILE__ == $0
 
   # Create directories for intemediate results
-  %w(pmids metadoc diseases).collect{|dir| File.join('work', dir)}.each{|dir|
+  %w(pmids metadocs diseases).collect{|dir| File.join('work', dir)}.each{|dir|
     FileUtils.mkdir_p dir unless File.exist? dir
   }
 
